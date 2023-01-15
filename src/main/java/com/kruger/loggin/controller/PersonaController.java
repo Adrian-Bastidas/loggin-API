a8e96ec696add9246afc01a51cb15374b5ca1a32
magobasti <abastidas94@gmail.com>
2023-01-14 21:12:12 -0500

    acceso con headers

--- src/main/java/com/kruger/loggin/controller/PersonaController.java
+++ src/main/java/com/kruger/loggin/controller/PersonaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 @RequestMapping("/usuarios")
 public class PersonaController {
 
